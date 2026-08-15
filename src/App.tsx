771fe3b6c290858e01569c299238962b935bb9ca
MI-Mahin <mahmudulislammahin07@gmail.com>
2026-08-15 20:07:30 +0600

    Fix: Resize for mobile responsiveness

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import Navbar from './components/Navbar';
 import ThreeBackground from './components/ThreeBackground';
 import Hero3D from './components/Hero3D';
-import About from './components/About';
 import Education from './components/Education';
 import Experience from './components/Experience';
 import Projects from './components/Projects';
@@ -16,7 +15,6 @@ function App() {
       <ThreeBackground />
       <Navbar />
       <Hero3D />
-      <About />
       <Experience />
       <Education />
       <Projects />
